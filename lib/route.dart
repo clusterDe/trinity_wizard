53d6111792c241112c3b8bb758b44f55345311e8
clusterDhe <dhannydhinata@gmail.com>
2024-06-29 10:41:13 +0700

    add home to route

--- lib/route.dart
+++ lib/route.dart
@@ -1,6 +1,15 @@
 import 'package:go_router/go_router.dart';
+import 'package:trinity_wizard/screen/home/home_screen.dart';
 
 GoRouter route = GoRouter(
   debugLogDiagnostics: true,
-  routes: [],
+  initialLocation: HomeScreen.routeName,
+  restorationScopeId: HomeScreen.routeName,
+  routes: [
+    GoRoute(
+      path: HomeScreen.routeName,
+      name: HomeScreen.routeName,
+      builder: (context, state) => const HomeScreen(),
+    ),
+  ],
 );
